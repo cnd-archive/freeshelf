2b0d0fcf050e04bcdba060b8f51595c429412dff
Clinton N. Dreisbach <crnixon@gmail.com>
2012-11-25 19:22:54 -0500

    Order books properly

--- freeshelf/views.py
+++ freeshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from books.models import Category, Book
 
 def home(request):
-    books = Book.for_free.all()
+    books = Book.for_free.order_by('-added_at').all()
     categories = Category.objects.order_by('name').all()
     return render_to_response('index.html',
         {'books': books, 'categories': categories})
@@ -10,6 +10,6 @@ def home(request):
 def category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     categories = Category.objects.order_by('name').all()
-    books = category.books.filter(free=True).all()
+    books = category.books.filter(free=True).order_by('-added_at').all()
     return render_to_response('category.html',
         {'category': category, 'books': books, 'categories': categories})
